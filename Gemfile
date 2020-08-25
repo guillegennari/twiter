d82b99ebcc57bc7fa0935548f45d1014ef3e8d39
Guillermo <mitogennari@gmail.com>
2020-08-24 21:29:46 -0400

    commit with devise and layout

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'mini_magick', '~> 4.8'
 
 gem 'devise'
+gem 'kaminari'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
---
+++ app/models/tweet.rb
@@ -0,0 +1,11 @@
+class Tweet < ApplicationRecord
+  validates :content, presence: true
+ 
+  belongs_to :user
+  has_many :likes
+  has_many :liking_users, :through => :likes, :source => :user
+
+  
+
+  paginates_per 2
+end
