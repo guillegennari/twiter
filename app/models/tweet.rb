c914930f266321de0b3fe440c9207c97ccdbb06c
Guillermo <mitogennari@gmail.com>
2020-08-29 16:23:42 -0400

    agregar follow y followers y vista de tweets de amigos

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,6 +7,8 @@ class Tweet < ApplicationRecord
 
   paginates_per 10
 
+  scope :tweets_for_me, -> (user){where(user_id: user.friends.pluck(:friend_id))}
+
   def is_liked?(user)
     if self.liking_users.include?(user)
       true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  #get 'users/follow'
   resources :tweets do
   post 'likes', to: 'tweets#likes'
   post 'retweet', to: 'tweets#retweet'
@@ -6,6 +7,10 @@ Rails.application.routes.draw do
 
   devise_for :users
   get 'home/index'
+  get 'all_tweets', to: 'home#all_tweets', as: 'all_tweets'
+
+  #get "users#follow"
+  post 'follow/:user_id', to: 'users#follow', as: 'users_follow'
 
   root to: 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
